ecb2204d571a9f2f8bbb894d1f3ec60900cff605
Andrew Deveau <adeveau9@gmail.com>
2017-11-10 21:54:20 -0800

    Initial commit w/ multiprocessing

---
+++ convert.py
@@ -0,0 +1,27 @@
+from pydub import AudioSegment
+from multiprocessing import Pool
+import os
+import sys
+import glob
+
+def convert(pathname, size = 10):
+    try:
+        song = AudioSegment.from_mp3(pathname)
+        split_path = os.path.split(pathname)
+        os.mkdir(os.path.join(split_path[0], split_path[1][:-4]))
+        for i, start in enumerate(range(1, len(song), size*1000)):
+            cur_segment = song[start: start + size*1000]
+            cur_segment.export(os.path.join(split_path[0], split_path[1][:-4], "segment_{}.wav".format(i)), format = "wav")
+
+        return 0
+    except Execption as e:
+        with open("convert.log", "w+") as log:
+            log.write(pathname)
+            log.write(e)
+            
+
+if __name__ == "__main__":
+    paths = glob.glob("./**/*.mp3", recursive = True)
+    p = Pool(5)
+    p.map(convert, paths)
+ 
