09eafdb784d2c2623f4346608f364a683193c90e
Andrew Brian Deveau <adeveau@rice06.stanford.edu>
2017-11-13 17:02:24 -0800

    Running, but still a bit of refactoring to do

--- preprocessing.py
+++ preprocessing.py
@@ -3,31 +3,28 @@ from scipy.io import wavfile
 import glob
 import numpy as np
 import os
-from multiprocessing import Pool
+from pathos.multiprocessing import ProcessPool
+import sys
 
 def spectrogram(pathname):
     samples = wavfile.read(pathname)
     t, f, Sxx = signal.spectrogram(samples[1])
     return np.ndarray.flatten(np.transpose(Sxx))
 
-def build_data():
-    languages_key = {'arabic':0, 'english_american':1, 'french':2, 'chinese':3,
-                    'hebrew':4, 'korean':5, 'russian':6, 'spanish':7}
-
-    data = p.map(glob.glob("./arabic/**/*.wav", recursive = True), parse)
-    X = np.vstack([tup[0] for tup in data if len(tup[0]) == 126936])
-    y = np.array(tup[1] for tup in data if len(tup[0]) == 126936)
-
-    return X, y
+def build_data(language):
+    p = ProcessPool(5)
+    data = p.map(parse, glob.glob("./{}/**/*.wav".format(language), recursive = True)) 
+    X = np.vstack([spect for spect in data if len(spect) == 126936])
+    return X
 
 def parse(path):
-
     lang = os.path.normpath(path).split(os.sep)[0]
-
     x = spectrogram(path)
-    y = languages_key[lang]
-
-    return x, y
+    return x
 
 if __name__ == "__main__":
-    X, Y = build_data()
+    lang = sys.argv[1]
+    X = build_data(lang)
+    print(X.shape)
+    np.save("{}_features.npy".format(lang), X)
+
