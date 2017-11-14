06e94f56b671b597a0490038e959abe7f98c5e9a
Andrew Deveau <adeveau9@gmail.com>
2017-11-13 16:15:24 -0800

    Try parallelizing preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -3,7 +3,7 @@ from scipy.io import wavfile
 import glob
 import numpy as np
 import os
-
+from multiprocessing import Pool
 
 def spectrogram(pathname):
     samples = wavfile.read(pathname)
@@ -14,19 +14,20 @@ def build_data():
     languages_key = {'arabic':0, 'english_american':1, 'french':2, 'chinese':3,
                     'hebrew':4, 'korean':5, 'russian':6, 'spanish':7}
 
-    X, Y = [], []
-    for path in glob.glob("./arabic/**/*.wav", recursive = True):
-        print(path)
-        lang = os.path.normpath(path).split(os.sep)[0]
+    data = p.map(glob.glob("./arabic/**/*.wav", recursive = True), parse)
+    X = np.vstack([tup[0] for tup in data if len(tup[0]) == 126936])
+    y = np.array(tup[1] for tup in data if len(tup[0]) == 126936)
 
-        x = spectrogram(path)
-        y = languages_key[lang]
+    return X, y
 
-        X.append(x)
-        Y.append(y)
+def parse(path):
 
+    lang = os.path.normpath(path).split(os.sep)[0]
 
-    return np.array(X), np.array(Y)
+    x = spectrogram(path)
+    y = languages_key[lang]
+
+    return x, y
 
 if __name__ == "__main__":
     X, Y = build_data()
