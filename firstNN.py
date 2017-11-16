c4c313c43eff832a74b7c5bd2e4498f6cb151c95
julienboussard <julienboussard@gmail.com>
2017-11-16 15:38:09 -0800

    Update firstNN.py

--- firstNN.py
+++ firstNN.py
@@ -1,5 +1,4 @@
 from keras.models import Sequential
-
 # Import `Dense` from `keras.layers`, fully connected NN
 from keras.layers import Dense
 
@@ -8,11 +7,9 @@ model = Sequential()
 
 # Add an input layer 
 model.add(Dense(12, activation='relu', input_shape=(13,)))
-
 # Add two hidden layer 
 model.add(Dense(8, activation='relu'))
 model.add(Dense(4, activation='relu'))
-
 # Add an output layer 
 model.add(Dense(1, activation='sigmoid'))
 
@@ -26,12 +23,10 @@ model.add(Dense(1, activation='sigmoid'))
 # model.get_weights()
 
 model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
-                   
-model.fit(X_train, y_train,epochs=20, batch_size=1, verbose=1)
 
-y_pred = model.predict(X_test)
-
-score = model.evaluate(X_test, y_test,verbose=1)
-
-return score
+def neural_net(X_train, y_train, X_test, y_test):
+  model.fit(X_train, y_train,epochs=20, batch_size=1, verbose=1)
+  y_pred = model.predict(X_test)
+  score = model.evaluate(X_test, y_test,verbose=1)
+  return score
 
