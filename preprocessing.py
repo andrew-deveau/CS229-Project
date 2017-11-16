8422146bbaf60cff91f222c781f39931c9f90b62
Andrew Brian Deveau <adeveau@rice16.stanford.edu>
2017-11-16 15:57:43 -0800

    Add MFCC coefs

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,11 @@ import numpy as np
 import os
 from pathos.multiprocessing import ProcessPool
 import sys
+from python_speech_features.base import mfcc
+
+def calc_mfcc(pathname):
+    samprate, samples = wavfile.read(pathname)
+    return mfcc(samples, samplerate = samprate, appendEnergy = False, nfft = 1024) 
 
 def spectrogram(pathname):
     samples = wavfile.read(pathname)
@@ -14,12 +19,12 @@ def spectrogram(pathname):
 def build_data(language):
     p = ProcessPool(5)
     data = p.map(parse, glob.glob("./{}/**/*.wav".format(language), recursive = True)) 
-    X = np.vstack([spect for spect in data if len(spect) == 126936])
+    X = np.vstack(data)
     return X
 
 def parse(path):
     lang = os.path.normpath(path).split(os.sep)[0]
-    x = spectrogram(path)
+    x = calc_mfcc(path)
     return x
 
 if __name__ == "__main__":
