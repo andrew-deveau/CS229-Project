c37e819b106be361a3a2fb877fc530edcd6abcfb
julienboussard <julienboussard@gmail.com>
2017-11-16 15:32:00 -0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -5,12 +5,20 @@ import numpy as np
 import os
 from pathos.multiprocessing import ProcessPool
 import sys
+from python_speech_features import mfcc
+from python_speech_features import logfbank
+
 
 def spectrogram(pathname):
     samples = wavfile.read(pathname)
     t, f, Sxx = signal.spectrogram(samples[1])
     return np.ndarray.flatten(np.transpose(Sxx))
 
+def mfcc(pathnam):
+    samples = wavfile.read(pathname)
+    mfcc_feat = mfcc(samples[1],samples[0], winlen=0.025, winstep=0.01)
+    return mfcc_feat
+    
 def build_data(language):
     p = ProcessPool(5)
     data = p.map(parse, glob.glob("./{}/**/*.wav".format(language), recursive = True)) 
