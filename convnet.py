483f4050d070dea9fa2671a176300c1a1bb8fcc3
Andrew Brian Deveau <adeveau@rice14.stanford.edu>
2017-12-04 15:37:40 -0800

    Command line args for convnet

--- convnet.py
+++ convnet.py
@@ -4,18 +4,17 @@ import pickle
 from keras.utils.np_utils import to_categorical
 from keras.optimizers import Adam
 import numpy as np
-
+import sys
 
 def load(path):
     with open(path, 'rb+') as f:
         return pickle.load(f)
 
-def load_data(n, which = 'train', languages = ['english', 'french']):
+def load_data(n, languages, which = 'train') :
     path = "/farmshare/user_data/adeveau/CS229-Project/features/{}/ungrouped/".format(which)
     data = {}
-    
-    data['english'] = load(path+'english_{}_features.pkl'.format(which))
-    data['french'] = load(path+'french_{}_features.pkl'.format(which))
+    for lang in languages:
+        data[lang] = load(path+'{}_{}_features.pkl'.format(lang, which))
    
     for lang in data:
         data[lang] = [x[:n,:] for x in data[lang] if x.shape[0] > n]
@@ -40,17 +39,25 @@ def load_data(n, which = 'train', languages = ['english', 'french']):
     y = to_categorical(y)
     return X, y
 
-def model_constr(n):
+def model_constr(n, n_classes):
     model = Sequential()
     model.add(Conv2D(5, (10,1),  input_shape = (300, 13, 1), kernel_initializer='he_normal', activation='sigmoid'))
     model.add(Flatten())
-    model.add(Dense(units = 2, activation = 'softmax'))
+    model.add(Dense(units = n_classes, activation = 'softmax'))
     model.compile(loss = 'categorical_crossentropy', optimizer = Adam(), metrics = ['accuracy'])
     return model
 
 
 if __name__ == '__main__':
-    X, y = load_data(300)
+    if len(sys.argv) > 1:
+        n = int(sys.argv[1])
+    else:
+        n = 300
+    if len(sys.argv) > 2:
+        langs = sys.argv[2:]
+    else:
+        langs = ['english', 'french']
+    X, y = load_data(n, languages = langs)
     print(X.shape, y.shape)
-    model = model_constr(300)
+    model = model_constr(n, len(langs))
     model.fit(X, y, epochs = 5, batch_size = 10)
