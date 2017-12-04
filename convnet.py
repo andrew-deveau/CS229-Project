d91a31c8fcb9944f603c40a4152f07746e91b949
Andrew Brian Deveau <adeveau@rice14.stanford.edu>
2017-12-04 15:29:16 -0800

    Remove hard-coded parameters

--- convnet.py
+++ convnet.py
@@ -10,29 +10,35 @@ def load(path):
     with open(path, 'rb+') as f:
         return pickle.load(f)
 
-def load_data(n):
-    path = "/farmshare/user_data/adeveau/CS229-Project/features/train/ungrouped/"
-    train = {}
-    train['english'] = load(path+'english_train_features.pkl')
-    train['french'] = load(path+'french_train_features.pkl')
-   
-    for lang in train:
-        train[lang] = [x[:n,:] for x in train[lang] if x.shape[0] > n]
+def load_data(n, which = 'train', languages = ['english', 'french']):
+    path = "/farmshare/user_data/adeveau/CS229-Project/features/{}/ungrouped/".format(which)
+    data = {}
     
-    m = len(train['french'])
+    data['english'] = load(path+'english_{}_features.pkl'.format(which))
+    data['french'] = load(path+'french_{}_features.pkl'.format(which))
+   
+    for lang in data:
+        data[lang] = [x[:n,:] for x in data[lang] if x.shape[0] > n]
+    
+    m = min(len(data[lang]) for lang in data)
+    for lang in data:
+        data[lang] = data[lang][:m]
 
-    train['english'] = train['english'][:m]
+    for lang in data:
+        data[lang] = np.dstack(data[lang])
+        data[lang] = np.swapaxes(data[lang], 0,2)
+        data[lang] = np.reshape(data[lang], (m, n, 13, 1))
+    y = []
+    for i,lang in enumerate(data):
+        try:
+            X = np.concatenate((X,data[lang]), axis = 0)
+        except UnboundLocalError:
+            X = data[lang]
+        y.extend([i]*m)
 
-    for lang in train:
-        train[lang] = np.dstack(train[lang])
-        train[lang] = np.swapaxes(train[lang], 0,2)
-        train[lang] = np.reshape(train[lang], (m, n, 13, 1))
-
-    X_train = np.concatenate((train['english'], train['french']), axis = 0)
-    y_train = [0]*m+[1]*m
-    y_train = np.array(y_train)
-    y_train = to_categorical(y_train)
-    return X_train, y_train
+    y = np.array(y)
+    y = to_categorical(y)
+    return X, y
 
 def model_constr(n):
     model = Sequential()
@@ -44,7 +50,7 @@ def model_constr(n):
 
 
 if __name__ == '__main__':
-    X_train, y_train = load_data(300)
-    print(X_train.shape, y_train.shape)
+    X, y = load_data(300)
+    print(X.shape, y.shape)
     model = model_constr(300)
-    model.fit(X_train, y_train, epochs = 5, batch_size = 10)
+    model.fit(X, y, epochs = 5, batch_size = 10)
